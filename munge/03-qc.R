75a4fedbed238659d228e2d911206a355d5690b2
Kenneth Daily <kmdaily@gmail.com>
2016-04-30 08:08:05 -0700

    deleted

--- munge/03-qc.R
+++
@@ -1,2 +0,0 @@
-## Do quality control on batch corrected data
-qc.all.batcheffect <- arrayQualityMetrics(expressionset=neweset.batcheffect, outdir="reports/QC/RMA.batcheffect.all/", force=T, intgroup="cancertype")
